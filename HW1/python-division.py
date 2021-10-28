3871e6458950f1d3c5d9b23039c44449253a33f7
PouriaDarandy <84618783+3OVeRLoRD13@users.noreply.github.com>
2021-10-28 09:56:21 +0330

    Add files via upload

---
+++ HW1/python-division.py
@@ -0,0 +1,10 @@
+a = int(input("Please enter the first number :"))
+b = int(input("Please enter the second number :"))
+
+c = a // b
+d = a / b
+
+print(f"a // b : {c}")
+print(f"a / b : {d}")
+
+quit_to_desk = input("enter anything to exit :")
